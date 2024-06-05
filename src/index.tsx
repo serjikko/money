9b93d7a692f7cec1262528adbd390fee23a9324a
SK <kononenkoss@yandex.ru>
2024-06-05 21:07:50 +0300

    index

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+// import { createRoot } from "react-dom/client";
+import { Root } from "./app";
+
+const root: HTMLElement = document.createElement("div");
+root.id = "root";
+root.className = "page";
+document.body.appendChild(root);
