bd9d5241702ddde59ced49bba362c4846905cd51
SK <kononenkoss@yandex.ru>
2024-06-05 21:19:38 +0300

    main

---
+++ src/app/main/main.tsx
@@ -0,0 +1,21 @@
+
+import { AddExpenses } from "../../components/add-expenses";
+import { AddIncome } from "../../components/add-income";
+import { WeeklyStats } from "../../components/weekly-stats/weekly-stats";
+
+import "./style.css";
+
+
+<main className="page__home">
+  <div className="container">
+    <h1>Expenses App</h1>
+  </div>
+  
+  <div className="home__wrapper">
+    <>
+      <AddExpenses />
+      <AddIncome />
+      <WeeklyStats />
+    </>
+  </div>
+</main>
